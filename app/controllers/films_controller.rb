3183025094c73c3f96907733179f5ec04ca75c99
MonicaGrages <mgrages1@gmail.com>
2017-06-26 15:38:49 -0400

    complete film edit route

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -4,5 +4,29 @@ class FilmsController < ApplicationController
     @films = Film.all
   end
 
+  def show
+    @film = Film.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  def create
+    @film = Film.create
+  end
+
+  def update
+    @film = Film.find(params[:id])
+    @film.update(title: params["film"]["title"], year: params["film"]["year"], genre: params["film"]["genre"], synopsis: params["film"]["synopsis"], picture: params["film"]["picture"])
+    redirect_to film_path(@film)
+  end
+
+  def delete
+    @film = Find.find(params[:id])
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-get 'films' => 'films#index', as: 'films'
-root 'films#index'
+  get 'films' => 'films#index', as: 'films'
+  root 'films#index'
+  get 'films/new' => 'films#new', as: 'film_new'
+  get 'films/:id' => 'films#show', as: 'film'
+  get 'films/:id/edit' => 'films#edit', as: 'film_edit'
+  post 'films' => 'films#create', as: 'film_create'
+  patch 'films/:id' => 'films#update', as: 'film_update'
+  delete 'films/:id' => 'films#delete', as: 'film_delete'
 
 end
