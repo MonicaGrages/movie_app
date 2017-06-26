9fb68cb9bbbd57653daabeec4e072f564a079842
MonicaGrages <mgrages1@gmail.com>
2017-06-26 16:20:06 -0400

    complete film edit route. start user model

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -9,6 +9,7 @@ class FilmsController < ApplicationController
   end
 
   def new
+    @film = Film.new
   end
 
   def edit
@@ -16,7 +17,8 @@ class FilmsController < ApplicationController
   end
 
   def create
-    @film = Film.create
+    @film = Film.create(title: params["film"]["title"], year: params["film"]["year"], genre: params["film"]["genre"], synopsis: params["film"]["synopsis"], picture: params["film"]["picture"])
+    redirect_to films_path
   end
 
   def update
@@ -26,7 +28,9 @@ class FilmsController < ApplicationController
   end
 
   def delete
-    @film = Find.find(params[:id])
+    @film = Film.find(params[:id])
+    @film.delete
+    redirect_to films_path
   end
 
 end
