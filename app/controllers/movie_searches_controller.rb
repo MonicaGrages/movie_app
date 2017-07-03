9c722d4b92af1ec3e3bfb07fc8c2efcb93b0a9b9
MonicaGrages <mgrages1@gmail.com>
2017-07-02 21:11:15 -0400

    added no results yielded message for movie search

--- app/controllers/movie_searches_controller.rb
+++ app/controllers/movie_searches_controller.rb
@@ -2,17 +2,15 @@ class MovieSearchesController < ApplicationController
 
   def index
     if params["/movie_search"]
-      puts "search_params is: "
-      puts search_params.to_h
       search_term = search_params.to_h[:search_term]
-      puts search_term
       @movies = MovieSearch.search(search_term)
       puts @movies
-      @movies_array = @movies["Search"]
-      puts @movies_array.length
-      @movie_results = @movies_array.paginate(page: params[:page], per_page: 5)
-      puts @movie_results.class
-      puts @movie_results
+      if @movies[:Response]
+        @movies_array = @movies["Search"]
+        @movie_results = @movies_array.paginate(page: params[:page], per_page: 5)
+      else
+        @no_results_message = "Your search yielded no results"
+      end
     end
   end
 
